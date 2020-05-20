388d96ff5cb9f0ee3187e63a83ac1f569b47136e
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-20 01:56:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,23 @@ class Triangle
   
   def kind 
     if (@side_a + @side_b) < @side_c || (@side_b + @side_c) < @side_a || (@side_a + @side_c) < @side_b || @side_a >= 0 || @side_b >= 0 || @side_c >= 0 
+      begin 
+        raise TriangleError
+      rescue TriangleError => error 
+          puts error.message 
+      end 
+    elsif @side_a == @side_b && @side_b == @side_c 
+      "equalaterial"
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      "isosceles"
+    else 
+      "scalene"
+    end 
   end 
   
   class TriangleError < StandardError
-    
+    def message 
+      "You smooth brain"
+    end 
   end 
 end
